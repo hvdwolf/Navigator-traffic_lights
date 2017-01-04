acc9a9ece67cba02d4ae20bf9eb1a9536c035356
hvdwolf <hvdwolf@gmail.com>
2017-01-04 11:14:23 +0100

    Update traffic_signals.py

--- traffic_signals.py
+++ traffic_signals.py
@@ -49,6 +49,10 @@ countries = ["albania", "andorra", "austria", "azores", "belarus", "belgium", "b
 #region = "south-america"
 #countries = ["argentina", "bolivia", "brazil", "chile", "colombia", "ecuador", "paraguay", "peru", "suriname", "uruguay"]
 
+### Asia
+#region = "asia"
+#countries = ["afghanistan", "azerbaijan", "bangladesh", "cambodia", "china", "gcc-states", "india", "indonesia", "iran", "iraq", "israel-and-palestine", "japan", "jordan", "kazakhstan", "kyrgyzstan", "lebanon", "malaysia-singapore-brunei", "maldives", "mongolia", "myanmar", "nepal", "north-korea", "pakistan", "philippines", "south-korea", "sri-lanka", "syria", "taiwan", "tajikistan", "thailand", "turkmenistan", "uzbekistan", "vietnam", "yemen"]
+
 # small test country
 #region = "europe"
 #countries = ["luxembourg"]
